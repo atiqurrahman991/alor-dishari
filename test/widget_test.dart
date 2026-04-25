81142b65d56f8290d70482f68c6d3b4ce40730ea
Alor Dishari Admin <admin@alordishari.com>
2026-04-25 23:33:07 +0600

    Fixed widget test and verified code health

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:alor_dishari/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ProviderScope(child: AlorDishariApp()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
